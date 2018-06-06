5491bc3d87dc4aacd7a496ee8faa26cc56e3f62a
criny <criny.master@gmail.com>
2018-06-06 13:22:54 +0900

    trimming

--- MultiWii/Serial.cpp
+++ MultiWii/Serial.cpp
@@ -4,45 +4,40 @@
 #include "Serial.h"
 #include "MultiWii.h"
 
-//crinyhere
+static volatile uint8_t serialHeadRX[UART_NUMBER], serialTailRX[UART_NUMBER];
+static uint8_t serialBufferRX[RX_BUFFER_SIZE][UART_NUMBER];
+static volatile uint8_t serialHeadTX[UART_NUMBER], serialTailTX[UART_NUMBER];
+static uint8_t serialBufferTX[TX_BUFFER_SIZE][UART_NUMBER];
 
-static volatile uint8_t serialHeadRX[1], serialTailRX[1];
-static uint8_t serialBufferRX[256][1];
-static volatile uint8_t serialHeadTX[1], serialTailTX[1];
-static uint8_t serialBufferTX[128][1];
-extern "C" void __vector_19(void) __attribute__ ((signal,used, externally_visible));
-
-void __vector_19(void)
-{
+ISR(USART_UDRE_vect)
+{  // Serial 0 on a PROMINI
 	uint8_t t = serialTailTX[0];
 
 	if (serialHeadTX[0] != t)
 	{
-		if (++t >= 128)
-			t = 0;
-		(*(volatile uint8_t *) (0xC6)) = serialBufferTX[t][0];
+		if (++t >= TX_BUFFER_SIZE) t = 0;
+		UDR0 = serialBufferTX[t][0];  // Transmit next byte in the ring
 		serialTailTX[0] = t;
 	}
-	if (t == serialHeadTX[0])
-		(*(volatile uint8_t *) (0xC1)) &= ~(1 << 5);
+	if (t == serialHeadTX[0]) UCSR0B &= ~(1<<UDRIE0); // Check if all data is transmitted . if yes disable transmitter UDRE interrupt
 }
 
 void UartSendData(uint8_t port)
 {
-	(*(volatile uint8_t *) (0xC1)) |= (1 << 5);
+	UCSR0B |= (1 << UDRIE0);
 }
 
 void SerialOpen(uint8_t port, uint32_t baud)
 {
-	uint8_t h = ((16000000L / 4 / baud - 1) / 2) >> 8;
-	uint8_t l = ((16000000L / 4 / baud - 1) / 2);
+	uint8_t h = ((F_CPU / 4 / baud - 1) / 2) >> 8;
+	uint8_t l = ((F_CPU / 4 / baud - 1) / 2);
 	switch (port)
 	{
 	case 0:
-		(*(volatile uint8_t *) (0xC0)) = (1 << 1);
-		(*(volatile uint8_t *) (0xC5)) = h;
-		(*(volatile uint8_t *) (0xC4)) = l;
-		(*(volatile uint8_t *) (0xC1)) |= (1 << 4) | (1 << 3) | (1 << 7);
+		UCSR0A = (1 << U2X0);
+		UBRR0H = h;
+		UBRR0L = l;
+		UCSR0B |= (1 << RXEN0) | (1 << TXEN0) | (1 << RXCIE0);
 		break;
 	}
 }
@@ -52,25 +47,24 @@ void SerialEnd(uint8_t port)
 	switch (port)
 	{
 	case 0:
-		(*(volatile uint8_t *) (0xC1)) &= ~((1 << 4) | (1 << 3) | (1 << 7) | (1 << 5));
+		UCSR0B &= ~((1 << RXEN0) | (1 << TXEN0) | (1 << RXCIE0) | (1 << UDRIE0));
 		break;
 	}
 }
 
+// we don't care about ring buffer overflow (head->tail) to avoid a test condition : data is lost anyway if it happens
 void store_uart_in_buf(uint8_t data, uint8_t portnum)
 {
 	uint8_t h = serialHeadRX[portnum];
 	serialBufferRX[h++][portnum] = data;
-	if (h >= 256)
+	if (h >= RX_BUFFER_SIZE)
 		h = 0;
 	serialHeadRX[portnum] = h;
 }
 
-extern "C" void __vector_18(void) __attribute__ ((signal,used, externally_visible));
-
-void __vector_18(void)
+ISR(USART_RX_vect)
 {
-	store_uart_in_buf((*(volatile uint8_t *) (0xC6)), 0);
+	store_uart_in_buf(UDR0, 0);
 }
 
 uint8_t SerialRead(uint8_t port)
@@ -79,7 +73,7 @@ uint8_t SerialRead(uint8_t port)
 	uint8_t c = serialBufferRX[t][port];
 	if (serialHeadRX[port] != t)
 	{
-		if (++t >= 256)
+		if (++t >= RX_BUFFER_SIZE)
 			t = 0;
 		serialTailRX[port] = t;
 	}
@@ -88,18 +82,18 @@ uint8_t SerialRead(uint8_t port)
 
 uint8_t SerialAvailable(uint8_t port)
 {
-	return ((uint8_t)(serialHeadRX[port] - serialTailRX[port])) % 256;
+	return ((uint8_t)(serialHeadRX[port] - serialTailRX[port])) % RX_BUFFER_SIZE;
 }
 
 uint8_t SerialUsedTXBuff(uint8_t port)
 {
-	return ((uint8_t)(serialHeadTX[port] - serialTailTX[port])) % 128;
+	return ((uint8_t)(serialHeadTX[port] - serialTailTX[port])) % TX_BUFFER_SIZE;
 }
 
 void SerialSerialize(uint8_t port, uint8_t a)
 {
 	uint8_t t = serialHeadTX[port];
-	if (++t >= 128)
+	if (++t >= TX_BUFFER_SIZE)
 		t = 0;
 	serialBufferTX[t][port] = a;
 	serialHeadTX[port] = t;
